c7dc4e940f2472b214343cbf52a4e00090b1e859
flaribbit <1149761294@qq.com>
2022-05-23 19:55:22 +0800

    修复重复单词和同音词问题 添加查询

--- WordDict.py
+++ WordDict.py
@@ -12,7 +12,9 @@ class WordDict:
         self.load('dict/N1.txt', 'N1')
 
     def find(self, text: str) -> List[str]:
-        pass
+        if text in self.data:
+            return self.data[text]
+        return []
 
     def load(self, filename: str, tag: str):
         with open(filename, 'r', encoding='utf-8') as f:
@@ -20,10 +22,20 @@ class WordDict:
                 row = line.rstrip().split('\t')
                 if row[0] not in self.data:
                     self.data[row[0]] = row+[tag]
+                if row[1] not in self.data:
+                    self.data[row[1]] = row+[tag]
+                else:
+                    if row[0] in self.data[row[1]][0]:  # 重复
+                        continue
+                    v = self.data[row[1]].copy()
+                    v[0] += '<br>'+row[0]
+                    v[3] += '<br>'+row[3]
+                    self.data[row[1]] = v
 
 
 if __name__ == '__main__':
     wd = WordDict()
-    # print(wd.find('あいうえお'))
-    for k, v in wd.data.items():
-        print(k, v)
+    print(wd.find('大きな'))
+    print(wd.find('おおきな'))
+    # for k, v in wd.data.items():
+    #     print(k, v)
