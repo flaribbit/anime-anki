a6d45c88f6c0d3a10c857f242313b692a999e486
flaribbit <1149761294@qq.com>
2022-05-23 20:29:13 +0800

    整理并生成卡片数据

--- main.py
+++ main.py
@@ -30,6 +30,30 @@ def split_words(text: str) -> List[str]:
     return res
 
 
-text = read_srt('subtitle/Gochuumon wa Usagi Desuka_ Bloom (01-12, JP only)/Gochuumon wa Usagi Desuka.S03E01.ja.srt')
-for line in text.split('\n'):
-    print(split_words(line))
+def get_gochiusa_cards():
+    from WordDict import WordDict
+    wordDict = WordDict()
+    cards = {}
+    for ep in range(1, 13):
+        print(f'processing EP{ep}')
+        filename = f'subtitle/Gochuumon wa Usagi Desuka_ Bloom (01-12, JP only)/Gochuumon wa Usagi Desuka.S03E{ep:02}.ja.srt'
+        text = read_srt(filename).replace('\u3000', ' ')
+        for line in text.split('\n'):
+            for word in split_words(line):
+                if word in cards:
+                    if f'E{ep:02}' in cards[word][4]:
+                        continue
+                    cards[word][4] += f' 点兔S03E{ep:02}'
+                    continue
+                res = wordDict.find(word)
+                if len(res) > 0:
+                    cards[word] = res+[line]  # 汉字，假名，声调，翻译，标签，原句
+                    cards[word][4] += f' 点兔S03E{ep:02}'
+                else:
+                    cards[word] = [word, '-', '-', '-', '-', line]
+    with open('anki/点兔S03.txt', 'w', encoding='utf-8') as f:
+        for word, v in cards.items():
+            f.write('\t'.join(v)+'\n')
+
+
+get_gochiusa_cards()
