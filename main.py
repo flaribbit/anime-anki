bfb03c2a8235674cbf7f97e67865fc0ac7253324
flaribbit <1149761294@qq.com>
2022-05-25 08:47:09 +0800

    添加魔女之旅字幕文件

--- main.py
+++ main.py
@@ -56,4 +56,31 @@ def get_gochiusa_cards():
             f.write('\t'.join(v)+'\n')
 
 
+def get_elaina_cards():
+    from WordDict import WordDict
+    wordDict = WordDict()
+    cards = {}
+    for ep in range(1, 13):
+        print(f'processing EP{ep}')
+        filename = f'subtitle/Wandering Witch_The Journey of Elaina/S01E{ep:02}.ja.srt'
+        text = read_srt(filename).replace('\u3000', ' ')
+        for line in text.split('\n'):
+            for word in split_words(line):
+                if word in cards:
+                    if f'E{ep:02}' in cards[word][4]:
+                        continue
+                    cards[word][4] += f' 魔女之旅E{ep:02}'
+                    continue
+                res = wordDict.find(word)
+                if len(res) > 0:
+                    cards[word] = res+[line]  # 汉字，假名，声调，翻译，标签，原句
+                    cards[word][4] += f' 魔女之旅E{ep:02}'
+                else:
+                    cards[word] = [word, '-', '-', '-', '-', line]
+    with open('anki/魔女之旅.txt', 'w', encoding='utf-8') as f:
+        for word, v in cards.items():
+            f.write('\t'.join(v)+'\n')
+
+
 get_gochiusa_cards()
+get_elaina_cards()
